f4d0386c6507c19b521f84412ad5a2b5326ab724
Anna Sokolova <asl@ansto.gov.au>
2018-03-09 15:16:27 +1100

    Update subtractions scripts; now subtracted data are loaded back to Mantid. Update reducer_example, fix bug with StartTime/StopTime values.

--- additional_scripts/subtraction/subtraction_final.py
+++ additional_scripts/subtraction/subtraction_final.py
@@ -12,13 +12,12 @@ reload (BilbyCustomFunctions_Reduction)
 # NOTE output files will be created in the same folder where "subtraction_list" sits
 # Folder containing "subtraction_list" file must be on Mantid path, set in  "File-> Manage User Directories"
 # IF subtracted data file exists, it will be re-written
-subtraction_list = FileFinder.getFullPath('subtraction_list.csv') # main list of files to be scaled and subtracted; folder must be on Mantid path
-index_files_to_subtract = ""                                                        # index(es) of pair to subtract
+subtraction_list = FileFinder.getFullPath('FileList_test_6255.csv') # main list of files to be scaled and subtracted; folder must be on Mantid path
+index_files_to_subtract = "0"                                                        # index(es) of pair to subtract
 
 # USER input end
 
 # =====================================================================================================
-
 # creating array of data from the input list
 parameters = BilbyCustomFunctions_Reduction.FilesListReduce(subtraction_list)
 files_to_subtract = BilbyCustomFunctions_Reduction.FilesToReduce(parameters, index_files_to_subtract)
@@ -32,6 +31,15 @@ for current_file in files_to_subtract:
     
     background_file = current_file["background"]    
     ws_bcgd_ini = LoadAscii(background_file)                                # load background data
+
+# check if the input files have the same number of points and contain only one spectrum
+    number_of_bins_sample = ws_sample_ini.blocksize()
+    number_of_spectra_sample =  ws_sample_ini.getNumberHistograms()
+    number_of_bins_bcgd = ws_bcgd_ini.blocksize()    
+    number_of_spectra_bcgd =  ws_bcgd_ini.getNumberHistograms()
+    if (number_of_spectra_sample > 1 or number_of_spectra_bcgd > 1):
+        raise ValueError("each data set must contain only one spectrum")    
+# end of check
         
     try:
         scale_subtr = float(current_file["scale_subtr"])                # const to subtract from the sample data
@@ -45,20 +53,8 @@ for current_file in files_to_subtract:
         
     ws_sample_const_sub = Scale(ws_sample_ini, scale_subtr, "Add")     # subtract const from the SAMPLE
     ws_bcgd_scaled = Scale(ws_bcgd_ini, scale_mult, "Multiply") # multiply BACKGROUND data by const
+    subtracted_data = Minus(ws_sample_const_sub, ws_bcgd_scaled, AllowDifferentNumberSpectra = True)
     
-    subtracted_data = Minus(ws_sample_const_sub, ws_bcgd_scaled)
-    
-# check if the input files have the same number of points and contain only one spectrum
-    number_of_bins_sample = ws_sample_ini.blocksize()
-    number_of_spectra_sample =  ws_sample_ini.getNumberHistograms()
-    number_of_bins_bcgd = ws_bcgd_ini.blocksize()    
-    number_of_spectra_bcgd =  ws_bcgd_ini.getNumberHistograms()    
-    if (number_of_bins_sample != number_of_bins_bcgd):
-        raise ValueError("number of data point in the sample and background data sets must be the same")    
-    if (number_of_spectra_sample > 1 or number_of_spectra_bcgd > 1):
-        raise ValueError("each data set must contain only one spectrum")    
-# end of check    
-
 # set-up the name of output file; name is created from a name of the original SAMPLE file
     sub_file_output_short = sample_file[0:(len(sample_file)-4)] + "_sub.dat"
     sub_file_output = os.path.join(os.path.dirname(subtraction_list), sub_file_output_short) # path for the output file, based on location of the initial list
@@ -75,22 +71,31 @@ for current_file in files_to_subtract:
     header_line.append(['background file name: ' + background_file])
     header_line.append(['Constant to subtract from sample scattering = ' + str(scale_subtr)])
     header_line.append(['Background multiplier = ' + str(scale_mult)])
-    print header_line
 # writing header in the file
     for line in header_line:
-        print line
         with open(sub_file_output, 'ab') as f_out:
             wr = csv.writer(f_out, delimiter=',', lineterminator='\n')
             wr.writerow(line)
-        
 
 # creating new file, where X, Y, ErrY are taken from scaled/subtracted data, but the Xerror - i.e. sigmaQ - are copied from the original sample data
     for i in range(number_of_bins_sample):
-        line_new_file = ["%8.5f" %subtracted_data.readX(0)[i],   "%8.5f" %subtracted_data.readY(0)[i], \
-                              "%8.5f" % subtracted_data.readE(0)[i], "%8.5f" %ws_sample_ini.readDx(0)[i]]
-        with open(sub_file_output, 'ab') as f_out:
-            wr = csv.writer(f_out, delimiter=',', lineterminator='\n')
-            wr.writerow(line_new_file)
+        s = str(subtracted_data.readY(0)[i])
+        if s.find("nan") == -1:  # skip lines with nan; need proper testing, need examples
+            line_new_file = ["%8.5f" %subtracted_data.readX(0)[i],   "%8.5f" %subtracted_data.readY(0)[i], \
+                                   "%8.5f" % subtracted_data.readE(0)[i], "%8.5f" %ws_sample_ini.readDx(0)[i]]
+            with open(sub_file_output, 'ab') as f_out:
+                wr = csv.writer(f_out, delimiter=',', lineterminator='\n')
+                wr.writerow(line_new_file)
+        #else:
+            #print "s nan" # temporary measure to see if some nan were found; to be deleted later
+
+# Just subtracted data are loaded back to Mantid
+# It is still a question if to keep it here
+# For good practice, make a function out of this script and make this parameter a user' choice
+    LoadAscii(sub_file_output, Unit = "MomentumTransfer", OutputWorkspace = sub_file_output)
 
 #============================================================================================================================================#
 #============================================================================================================================================#
+
+
+
