7f4d16bf448b4f56fbe1ba8cce3bb421297f553d
Anna Sokolova <asl@ansto.gov.au>
2018-03-21 16:30:23 +1100

    Change in logic of the subtracted file naming; new way is described in the script and in the example csv file

--- additional_scripts/subtraction/subtraction_final.py
+++ additional_scripts/subtraction/subtraction_final.py
@@ -13,7 +13,7 @@ reload (BilbyCustomFunctions_Reduction)
 # Folder containing "subtraction_list" file must be on Mantid path, set in  "File-> Manage User Directories"
 # IF subtracted data file exists, it will be re-written
 subtraction_list = FileFinder.getFullPath('list_example.csv') # main list of files to be scaled and subtracted; folder must be on Mantid path
-index_files_to_subtract = "1"                                                        # index(es) of pair to subtract
+index_files_to_subtract = "1,2"                                                        # index(es) of pair to subtract
 
 # USER input end
 
@@ -22,13 +22,13 @@ index_files_to_subtract = "1"                                                   
 parameters = BilbyCustomFunctions_Reduction.FilesListReduce(subtraction_list)
 files_to_subtract = BilbyCustomFunctions_Reduction.FilesToReduce(parameters, index_files_to_subtract)
 #mtd.clear()
+
 # reduce requested files one by one
 for current_file in files_to_subtract:                              
 
 # loading current sample + background pair
     sample_file = current_file["sample"]
     ws_sample_ini = LoadAscii(sample_file)                                 # load sample data
-    
     
     background_file = current_file["background"]    
     ws_bcgd_ini = LoadAscii(background_file)                                # load background data
@@ -43,21 +43,38 @@ for current_file in files_to_subtract:
 # end of check
         
     try:
-        scale_subtr = float(current_file["scale_subtr"])                # const to subtract from the sample data
-        scale_subtr = scale_subtr*(-1)                                          # needed because "Scale" knows only how to add        
+        scale_subtr_ini = float(current_file["scale_subtr"])             # const to subtract from the sample data
+        scale_subtr = scale_subtr_ini*(-1)                                          # needed because "Scale" knows only how to add        
     except:
+        scale_subtr_ini = 0.0                                                         # ini value is needed only for the logging into the result file, avoiding stuff like "-0"
         scale_subtr = 0.0                                                              # if there is no value in the input file, default value is 0
     try:
-        scale_mult = float(current_file["scale_mult"])                   # const to multiply background
+        scale_mult = float(current_file["scale_mult"])                # const to multiply background
     except:
         scale_mult = 1.0                                                               # if there is no value in the input file, default value is 1
-        
+
+    name_suffix = str(current_file["suffix"])                           # name of the output file name
+    if len(name_suffix) > 1:
+        name_suffix = "_" + name_suffix
+    else:
+        name_suffix = ""                                                               # just add _sub to the sample files name to construct the output name       
+
+    replaced_name = str(current_file["output_file_name"])         # name of the output file name
+    if len(replaced_name) == 0:
+        replaced_name = ""                                                               # just add _sub to the sample files name to construct the output name       
+
     ws_sample_const_sub = Scale(ws_sample_ini, scale_subtr, "Add")     # subtract const from the SAMPLE
     ws_bcgd_scaled = Scale(ws_bcgd_ini, scale_mult, "Multiply") # multiply BACKGROUND data by const
     subtracted_data = Minus(ws_sample_const_sub, ws_bcgd_scaled, AllowDifferentNumberSpectra = True)
     
-# set-up the name of output file; name is created from a name of the original SAMPLE file
-    sub_file_output_short = sample_file[0:(len(sample_file)-4)] + "_sub.dat"
+# set-up the name of output file; name is created from a name of the original SAMPLE file if "output_file_name" is empty
+# otherwise it is taking output_file_name as a name for the output file, adding ".dat" at the end
+   
+    if len(replaced_name) > 0:
+       sub_file_output_short = replaced_name + name_suffix + ".dat"
+    else:
+        sub_file_output_short = sample_file[0:(len(sample_file)-4)] + name_suffix + "_sub.dat"
+
     sub_file_output = os.path.join(os.path.dirname(subtraction_list), sub_file_output_short) # path for the output file, based on location of the initial list
     
     if os.path.exists(sub_file_output):            # check if it does exist; delete if yes - no avoid appending 
@@ -68,9 +85,9 @@ for current_file in files_to_subtract:
 
 # creating header from input data
     header_line = []
-    header_line.append(['sample file name: ' + sample_file])
-    header_line.append(['background file name: ' + background_file])
-    header_line.append(['Constant to subtract from sample scattering = ' + str(scale_subtr)])
+    header_line.append(['Sample file name: ' + sample_file])
+    header_line.append(['Background file name: ' + background_file])
+    header_line.append(['Constant to subtract from sample scattering = ' + str(scale_subtr_ini)])
     header_line.append(['Background multiplier = ' + str(scale_mult)])
 # writing header in the file
     for line in header_line:
@@ -90,7 +107,6 @@ for current_file in files_to_subtract:
                 wr.writerow(line_new_file)
         #else:
             #print "s nan" # temporary measure to see if some nan were found; to be deleted later
-    print number_of_bins_sample
 
 # Just subtracted data are loaded back to Mantid
 # It is still a question if to keep it here
