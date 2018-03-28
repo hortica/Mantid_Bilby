4660686977784af6b5882e9f1b26be544f1fdb8f
Anna Sokolova <asl@ansto.gov.au>
2018-03-28 12:51:00 +1100

    Add option to subtract only const; add ValueError for relevant scripts when index of settings line is wrong.

--- additional_scripts/subtraction/subtraction_final.py
+++ additional_scripts/subtraction/subtraction_final.py
@@ -13,45 +13,36 @@ reload (BilbyCustomFunctions_Reduction)
 # Folder containing "subtraction_list" file must be on Mantid path, set in  "File-> Manage User Directories"
 # IF subtracted data file exists, it will be re-written
 subtraction_list = FileFinder.getFullPath('list_example.csv') # main list of files to be scaled and subtracted; folder must be on Mantid path
-index_files_to_subtract = ""                                                        # index(es) of pair to subtract
+index_files_to_subtract = "2"                                                        # index(es) of pair to subtract
 
 # USER input end
 
 # =====================================================================================================
 # creating array of data from the input list
 parameters = BilbyCustomFunctions_Reduction.FilesListReduce(subtraction_list)
+
 files_to_subtract = BilbyCustomFunctions_Reduction.FilesToReduce(parameters, index_files_to_subtract)
 #mtd.clear()
-
+if len(files_to_subtract) == 0:
+    raise ValueError('Please check index_files_to_subtract; chosen one does not exist')    
+        
 # reduce requested files one by one
 for current_file in files_to_subtract:                              
 
 # loading current sample + background pair
     sample_file = current_file["sample"]
+    if sample_file == '':
+        raise ValueError('There is no sample name given. Seriously??')    
     ws_sample_ini = LoadAscii(sample_file)                                 # load sample data
-    
-    background_file = current_file["background"]    
-    ws_bcgd_ini = LoadAscii(background_file)                                # load background data
-
-# check if the input files have the same number of points and contain only one spectrum
     number_of_bins_sample = ws_sample_ini.blocksize()
     number_of_spectra_sample =  ws_sample_ini.getNumberHistograms()
-    number_of_bins_bcgd = ws_bcgd_ini.blocksize()    
-    number_of_spectra_bcgd =  ws_bcgd_ini.getNumberHistograms()
-    if (number_of_spectra_sample > 1 or number_of_spectra_bcgd > 1):
-        raise ValueError("each data set must contain only one spectrum")    
-# end of check
-        
+      
     try:
         scale_subtr_ini = float(current_file["scale_subtr"])             # const to subtract from the sample data
         scale_subtr = scale_subtr_ini*(-1)                                          # needed because "Scale" knows only how to add        
     except:
         scale_subtr_ini = 0.0                                                         # ini value is needed only for the logging into the result file, avoiding stuff like "-0"
         scale_subtr = 0.0                                                              # if there is no value in the input file, default value is 0
-    try:
-        scale_mult = float(current_file["scale_mult"])                # const to multiply background
-    except:
-        scale_mult = 1.0                                                               # if there is no value in the input file, default value is 1
 
     name_suffix = str(current_file["suffix"])                           # name of the output file name
     if len(name_suffix) > 1:
@@ -64,9 +55,27 @@ for current_file in files_to_subtract:
         replaced_name = ""                                                               # just add _sub to the sample files name to construct the output name       
 
     ws_sample_const_sub = Scale(ws_sample_ini, scale_subtr, "Add")     # subtract const from the SAMPLE
-    ws_bcgd_scaled = Scale(ws_bcgd_ini, scale_mult, "Multiply") # multiply BACKGROUND data by const
-    subtracted_data = Minus(ws_sample_const_sub, ws_bcgd_scaled, AllowDifferentNumberSpectra = True)
     
+    try:     # background file might not be given; if not given, multiplier is not needed; only const will be subtracted from the sample
+        background_file = current_file["background"]    
+        ws_bcgd_ini = LoadAscii(background_file)                                # load background data
+        # check if the input files have the same number of points and contain only one spectrum
+        number_of_bins_bcgd = ws_bcgd_ini.blocksize()    
+        number_of_spectra_bcgd =  ws_bcgd_ini.getNumberHistograms()
+        if (number_of_spectra_sample > 1 or number_of_spectra_bcgd > 1):
+            raise ValueError("each data set must contain only one spectrum")    
+        # end of check
+        try:
+            scale_mult = float(current_file["scale_mult"])                # const to multiply background
+        except:
+            scale_mult = 1.0                                                               # if there is no value in the input file, default value is 1
+        ws_bcgd_scaled = Scale(ws_bcgd_ini, scale_mult, "Multiply") # multiply BACKGROUND data by const  
+        subtracted_data = Minus(ws_sample_const_sub, ws_bcgd_scaled, AllowDifferentNumberSpectra = True)
+        background = "yes"
+    except:
+        subtracted_data = ws_sample_const_sub
+        background = "none"
+        
 # set-up the name of output file; name is created from a name of the original SAMPLE file if "output_file_name" is empty
 # otherwise it is taking output_file_name as a name for the output file, adding ".dat" at the end
    
@@ -76,21 +85,26 @@ for current_file in files_to_subtract:
         sub_file_output_short = sample_file[0:(len(sample_file)-4)] + name_suffix + "_sub.dat"
 
     sub_file_output = os.path.join(os.path.dirname(subtraction_list), sub_file_output_short) # path for the output file, based on location of the initial list
-    
-    if os.path.exists(sub_file_output):            # check if it does exist; delete if yes - no avoid appending 
-       os.remove(sub_file_output)                  # ??? to check how does it know the path here ???
-    if not os.path.exists(sub_file_output):     # check if it does exist; create if not - to prepare for data recording
-        file = open(sub_file_output, 'w+')                     
-        file.close()     
 
 # creating header from input data
-    header_line = []
-    header_line.append(['Sample file name: ' + sample_file])
-    header_line.append(['Background file name: ' + background_file])
+    header_line_first = (['Sample file name: ' + sample_file]) # record first line separately to be sure file is re-written
+    with open(sub_file_output, 'w+') as f_out:
+        wr = csv.writer(f_out, delimiter=',', lineterminator='\n')
+        wr.writerow(header_line_first)
+
+    header_line = [] # add further header lines
+
+    if background != "none":    
+        header_line.append(['Background file name: ' + background_file])
+    else:
+        header_line.append(['No background file given; only const subtracted'])            
+
     header_line.append(['Constant to subtract from sample scattering = ' + str(scale_subtr_ini)])
-    header_line.append(['Background multiplier = ' + str(scale_mult)])
-# writing header in the file
-    for line in header_line:
+
+    if background != "none":
+        header_line.append(['Background multiplier = ' + str(scale_mult)])
+
+    for line in header_line: # write the rest of the header in the file
         with open(sub_file_output, 'ab') as f_out:
             wr = csv.writer(f_out, delimiter=',', lineterminator='\n')
             wr.writerow(line)
@@ -105,17 +119,12 @@ for current_file in files_to_subtract:
             with open(sub_file_output, 'ab') as f_out:
                 wr = csv.writer(f_out, delimiter=',', lineterminator='\n')
                 wr.writerow(line_new_file)
-        #else:
-            #print "s nan" # temporary measure to see if some nan were found; to be deleted later
 
 # Just subtracted data are loaded back to Mantid
 # It is still a question if to keep it here
 # For good practice, make a function out of this script and make this parameter a user' choice
     LoadAscii(sub_file_output, Unit = "MomentumTransfer", OutputWorkspace = sub_file_output_short)
-
+    print "File saved", FileFinder.getFullPath(sub_file_output_short)
 
 #============================================================================================================================================#
 #============================================================================================================================================#
-
-
-
