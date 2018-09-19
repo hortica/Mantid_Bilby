6cd35f5ee691f9817e587d976c988e7d9a18a905
Anna Sokolova <asl@ansto.gov.au>
2018-09-19 14:18:25 +1000

    Update file names

---
+++ additional_scripts/subtraction/create_subtraction_list.py
@@ -0,0 +1,59 @@
+# 29 December 2017: to record list into a proper template, not just into one column
+import glob, csv, os
+
+## FIRST ONE
+
+# user input START =================================================================
+
+# Set path where data sit
+### straight '/' and '/' at the end of the line
+path_data = "D:/Mantid_github/additional_scripts/subtraction/examples/"
+
+# Set mask for files selection; for example: *bby*12*2.0*20.0*.dat
+mask = "*.dat"
+
+list_file_name = "list_example.csv"
+
+# NOTE: you will have file "selected_list.csv" in THE SAME folder where your initial data are; 
+# Remember, that for "subtraction_final.py" script it is recommended to set-up a new folder with input csv list, because all subtracted files will sit in there
+
+# user input END ===================================================================
+
+os.chdir(path_data)
+selected_list = glob.glob(mask)
+#print selected_list
+
+path_list_file_name = path_data+list_file_name
+print path_list_file_name
+#set-up output file
+if os.path.exists(path_list_file_name):           # check if it does exist; delete if yes - no avoid appending 
+    try:
+        os.remove(path_list_file_name)                
+    except:
+        raise ValueError("It seems the file with this name already exists and open; please close it to proceed, note it will be overwritten.")
+
+if not os.path.exists(path_list_file_name):
+    file = open(path_list_file_name, 'w+')
+    file.close()
+
+header_1 = ['index', 'sample', 'background', 'scale_subtr', 'scale_mult', 'output_file_name', 'suffix']
+
+header_2 = ['', 'sample scattering 1D ASCII file name',  \
+'background scattering 1D ASCII file name', \
+'const to be subtracted from the SAMPLE scattering data', \
+'const, BACKGROUND data will be multiplied on',\
+'Can be left empty. In this case name will be [sample + suffix + _sub.dat]. If this name is given, the output file will be [name + suffix]',\
+'Can be left empty. If given, will be added to the output name file. If the [output_file_name] is empty, the name will be [sample + suffix + _sub.dat]']
+
+with open(path_list_file_name, 'ab') as f_out:
+    wr = csv.writer(f_out, delimiter=',', lineterminator='\n')
+    wr.writerow(header_1)
+    wr.writerow(header_2)    
+
+index = 0
+for item in selected_list:
+    raw = [index, item]
+    index = index + 1
+    with open(path_list_file_name, 'ab') as f_out:
+        wr = csv.writer(f_out, delimiter=',', lineterminator='\n')
+        wr.writerow(raw)
---
+++ additional_scripts/transmission_fit.py
@@ -0,0 +1,58 @@
+import numpy as np
+import mantid.simpleapi
+from mantid.api import *
+
+#input parameters  ==============================================================================================
+#sample transmission, empty beam transmission, mask to select area to calculate transmissions
+transm_sam = "BBY0014167.tar"
+empty_beam = "BBY0014172.tar"
+mask_transm = "transmission_mask_6286.xml"
+
+#binning
+binning_wavelength = [3.0,8.5, 20.0]
+
+#load data  ======================================================================================================
+ws_sam_transm = LoadBBY(transm_sam)
+ws_mask_transm = LoadMask('Bilby', mask_transm) 
+ws_empty = LoadBBY(empty_beam)
+
+
+time_empty = float(ws_empty.run().getProperty("frame_count").value)                    # ratio is enough
+time_transm_sample = float(ws_sam_transm.run().getProperty("frame_count").value)      # ratio is enough
+
+#masking
+MaskDetectors(ws_sam_transm, MaskedWorkspace=ws_mask_transm) 
+MaskDetectors(ws_empty, MaskedWorkspace=ws_mask_transm) 
+
+#convert to wavelength, bin and sum ================================================================================
+#sample transmission
+ws_sam_wave=ConvertUnits(ws_sam_transm, 'Wavelength')
+ws_sam_transm_wave = Rebin(ws_sam_wave, Params=binning_wavelength, PreserveEvents=False)
+#ws_sam_transm_wave = SumSpectra(ws_sam_transm_wave)
+
+#scale to take into account data collection time
+scale_factor = time_empty/time_transm_sample
+ws_sam_transm_wave = Scale(ws_sam_transm_wave, scale_factor)
+
+#empty beam
+ws_empty_wave=ConvertUnits(ws_empty, 'Wavelength')
+ws_empty_wave = Rebin(ws_empty_wave, Params=binning_wavelength, PreserveEvents=False)
+#ws_empty_wave = SumSpectra(ws_empty_wave)
+
+    
+#Transmission  ===================================================================================================
+#Define set of ID of masked detectors - needed as input for CalculateTransmission
+InvertMask(InputWorkspace=ws_mask_transm, OutputWorkspace = "_ws")
+ws_tranMskInv = AnalysisDataService.retrieve("_ws")
+DetectorList = ExtractMask(InputWorkspace = ws_tranMskInv, OutputWorkspace = 'test')
+ws_tranROI = DetectorList[1]
+#Just to check
+size = DetectorList[1].size
+print size
+DeleteWorkspace('_ws')
+DeleteWorkspace('test')
+
+#Calculating transmission
+transm_S1_PS_PEO_IrCl3_formic_1 = CalculateTransmission(ws_sam_transm_wave, ws_empty_wave, TransmissionROI = ws_tranROI, FitMethod = 'Linear', PolynomialOrder = '3', OutputUnfittedData = True)
+#FitMethod = 'Polynomial', PolynomialOrder = '2'
+#FitMethod = 'Log' 'Linear'
