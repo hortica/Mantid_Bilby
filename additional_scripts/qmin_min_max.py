3553749dcd3f6328ab56c40336d11f95428bc77e
SOKOLOVA <asl@ansto.gov.au>
2021-09-16 14:26:24 +1000

    Change plots: add Matplotlib

--- additional_scripts/qmin_min_max.py
+++ additional_scripts/qmin_min_max.py
@@ -1,8 +1,11 @@
 #07 January 2016
 #23 October 2019
+#16 September 2021: new plots
 
 from mantid.simpleapi import *
-from pymantidplot.qtiplot import plot
+import matplotlib.pylab as plt
+from matplotlib.ticker import (MultipleLocator)
+
 import numpy as np
 import math
 
@@ -10,6 +13,7 @@ import math
 
 # Functions ====================================================================================
 # Q-calculation - equation only
+
 def q_value(wl, dist_x, L2_det):
    #calculates q for given L2, lambda and distance from the beam centreline
    return (4*np.pi/wl)*np.sin(0.5*np.arctan(dist_x / L2_det))
@@ -29,12 +33,12 @@ def curtain_q(idc, n, dx, separation, L2_curtain):
 
 #INPUT 
 # wavelengths =================================================================================
-wl_range = [6.0, 6.5]                # wavelength, Angstroms  
+
+wl_range = [2.0, 10.0]                # wavelength, Angstroms  
 
 assymetry_expected_horizontal_only = False # Added to be sure that no gaps are appearing on the data when half of the curtains are masked off
-assymetry_expected_vertical_only = False     # Added to be sure that no gaps are appearing on the data when half of the curtains are masked off
-Bragg_edge_suspected = False                       # Added to be sure that no short wavelengths (<4.5A) are considered for the samples wiht the Bragg edge
-
+assymetry_expected_vertical_only = False    # Added to be sure that no gaps are appearing on the data when half of the curtains are masked off
+Bragg_edge_suspected = False                      # Added to be sure that no short wavelengths (<4.5A) are considered for the samples wiht the Bragg edge
 
 #INPUT Geometry =============================================================================
 
@@ -63,6 +67,7 @@ direct_shadow_radius = direct_shadow/2
 print ('Direct beam shadow radius is %5.2f' % direct_shadow_radius, 'mm')
 
 # wavelength range ================================================================================
+
 wl_min = wl_range[0]
 if Bragg_edge_suspected:
     if (wl_min < 4.5 ):
@@ -72,16 +77,32 @@ if Bragg_edge_suspected:
 wl_max = wl_range[1]
 print ('Q (1/Angstrom) values for wavelengths range from ', wl_range, 'Angstroms')
 
+# setting up the plot ==============================================================================
+
+fig, q_range = plt.subplots(subplot_kw={'projection': 'mantid'})
+title = 'Wavelength range from ' + str(wl_min) + ' to ' + str(wl_max) + ' A'
+q_range.set_title(title)
+
+q_range.set_xlabel('q ($\\AA^{-1}$)')
+#q_range.xaxis.grid(True, which = 'minor')
+q_range.grid(color='green', linestyle='-.', linewidth = 0.25)
+
+plt.tick_params(axis='x', direction='in', which='minor')
+plt.tick_params(axis='x', labelsize = '7')
+#, plt.tick_params(axis='y', direction='in')
+q_range.xaxis.set_major_locator(MultipleLocator(0.1))
+#q_range.xaxis.set_major_formatter('{x:.0f}')
+q_range.xaxis.set_minor_locator(MultipleLocator(0.025))
+
+q_range.yaxis.set_ticklabels([])
+
 # Q calculations ===================================================================================
 # REAR detector
+
 print ('============ Rear detector ============')
-#max_width  = math.sqrt((separation_u + panel_width) **2 + panel_width**2)
-#print 'bs, direct_shadow, direct_shadow_radius'
-#print bs, direct_shadow, direct_shadow_radius
 
 if direct_shadow_radius >  bs: # if chosen bs is large, bigger than the shadow
    bs = direct_shadow_radius
-   #print bs
    
 n = 118.0 - math.floor(bs / 2.81) #118*2.81 ~= 331.6mm
 nn = int(n)
@@ -89,20 +110,14 @@ separation_rear = bs
 
 ws_max, delta_step_wl_max, ws_min, delta_step_wl_min = curtain_q('rear', nn, 0.0, separation_rear, L2_det)
 
-#print mtd.getObjectNames()
-
 print('Qmin for rear and Step for max wavelength: %5.4f %5.4f' % (ws_max.readX(0)[0], delta_step_wl_max))
 print('Qmax for rear and Step for min wavelength: %5.4f %5.4f' % (ws_min.readX(0)[nn-1] , delta_step_wl_min))
 
-#plot([ws_max,ws_min],  [0], color='green', hold='on')
-#print l.numCurves()
-#l.removeCurve(0)
-
-#Plot section below needs cleaning up
-g = plot([ws_max, ws_min],  0, type=Layer.Line) 
-l = g.activeLayer()
-l.setTitle('Q range for rear, top, bottom, left, right curtains')
-l.showGrid()
+xr = [ws_max.readX(0)[0], ws_min.readX(0)[nn-1]]
+yr = np.ones(2)
+q_range.plot(xr, yr, 'x--', label='Rear detector')
+q_range.legend()
+fig.show()
 
 #CURTAIN detector ================================================================================
 n = 118 #330mm divided by 2.8mm pixel size
@@ -116,9 +131,11 @@ if assymetry_expected_horizontal_only is False:
     print('Qmin for top curtain and Step for max wavelength: %5.4f %5.4f' % (ws_max.readX(0)[0], delta_step_wl_max))
     print('Qmax for top curtain and Step for min wavelength: %5.4f %5.4f' % (ws_min.readX(0)[n-1] , delta_step_wl_min))
 
-    g1 = plot([ws_max,ws_min],  [0])
-    g = mergePlots (g, g1)
-    #c - how to make it work properly??
+    xt = [ws_max.readX(0)[0], ws_min.readX(0)[n-1]]
+    yt = 2 * np.ones(2)
+    q_range.plot(xt, yt, 'x--', label='Top curtain')
+    q_range.legend()
+    fig.show()
 
 # -------------------------------------------------------------------------------------------------------------------------------------------------
     print ('================= Bottom Curtain ===================')
@@ -128,20 +145,26 @@ if assymetry_expected_horizontal_only is False:
     print('Qmin for bottom curtain and Step for max wavelength: %5.4f %5.4f' % (ws_max.readX(0)[0], delta_step_wl_max))
     print('Qmax for bottom curtain and Step for min wavelength: %5.4f %5.4f' % (ws_min.readX(0)[n-1] , delta_step_wl_min))
 
-    g1 = plot([ws_max,ws_min],  [0])
-    g = mergePlots (g, g1)
+    xb = [ws_max.readX(0)[0], ws_min.readX(0)[n-1]]
+    yb = 3 * np.ones(2)
+    q_range.plot(xb, yb, 'x--', label='Bottom curtain')
+    q_range.legend()
+    fig.show()
 
 # -------------------------------------------------------------------------------------------------------------------------------------------------
 if assymetry_expected_vertical_only is False:
     print ('================= Left Curtain ===================')
 
     ws_max, delta_step_wl_max, ws_min, delta_step_wl_min = curtain_q('curtain_l', n, 1.5, separation_l, L2_curtain_lr)
-    print('Qmin for left curtain and Step for max wavelength: %5.4f %5.4f' % (ws_max.readX(0)[0], delta_step_wl_max))
 
+    print('Qmin for left curtain and Step for max wavelength: %5.4f %5.4f' % (ws_max.readX(0)[0], delta_step_wl_max))
     print('Qmax for left curtain and Step for min wavelength: %5.4f %5.4f' % (ws_min.readX(0)[n-1] , delta_step_wl_min))
 
-    g1 = plot([ws_max,ws_min],  [0])
-    g = mergePlots (g, g1)
+    xl = [ws_max.readX(0)[0], ws_min.readX(0)[n-1]]
+    yl = 4 * np.ones(2)
+    q_range.plot(xl, yl, 'x--', label='Left curtain')
+    q_range.legend()
+    fig.show()
 
 # -------------------------------------------------------------------------------------------------------------------------------------------------
     print ('================= Right Curtain ==================')
@@ -151,5 +174,8 @@ if assymetry_expected_vertical_only is False:
     print('Qmin for right curtain and Step for max wavelength: %5.4f %5.4f' % (ws_max.readX(0)[0], delta_step_wl_max))
     print('Qmax for right curtain and Step for min wavelength: %5.4f %5.4f' % (ws_min.readX(0)[n-1] , delta_step_wl_min))
 
-    g1 = plot([ws_max,ws_min],  [0])
-    g = mergePlots (g, g1)
+    xr = [ws_max.readX(0)[0], ws_min.readX(0)[n-1]]
+    yr = 5 * np.ones(2)
+    q_range.plot(xr, yr, 'x--', label='Right curtain')
+    q_range.legend()
+    fig.show()
